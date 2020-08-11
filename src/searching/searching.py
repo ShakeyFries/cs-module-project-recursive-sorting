2fcd233c2c4dc72dde606e5c3c01e979a0212b95
Chris Daley <Daleys@Chriss-iMac.local>
2020-08-11 15:00:12 -0600

    finished searching py

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,27 @@
 # TO-DO: Implement a recursive implementation of binary search
+# using code from Geek for geeks: https://www.geeksforgeeks.org/recursive-insertion-sort/
 def binary_search(arr, target, start, end):
-    # Your code here
+    # binary search algorithm
+    # base case
+    # start is >= the end
 
+    if end >= start:
+        # establish middle
+        middle = (start + end) // 2
+
+        # if target is pointing at the middle then return middle
+        if arr[mid] == target:
+            return mid
+
+        # if target is < middle then pivot left
+        elif arr[mid] > target:
+            return binary_search(arr, target, start, middle - 1)
+
+        # if target is >= to middle then pivot right.
+        else:
+            return binary_search(arr, target, middle +1, end)
+    else:
+        return -1
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
